f647846e06ee5ded94d2cb7bbe595bb05eacc38f
Albertomanas <amanas@cifpfbmoll.eu>
2020-02-12 01:45:40 +0100

    Test(EstacionTest): Caso Test crearEstacion

--- Bicipalma/src/test/java/edu/elsmancs/Bicipalma/domain/EstacionTest.java
+++ Bicipalma/src/test/java/edu/elsmancs/Bicipalma/domain/EstacionTest.java
@@ -7,10 +7,10 @@ public class EstacionTest {
 
     @Test
     public void crearEstacionTest(){
-        Estacion estacion = new Estacion(1, "Calle  Gaspar", 4);
+        Estacion estacion = new Estacion(1, "Manacor", 6);
         assertEquals(estacion.getNumEstacion(), 1, 0);
-        assertEquals(estacion.getDireccion(), "Calle Gaspar", 0);
-        assertEquals(estacion.getNumeroAnclajes(), 4);
+        assertEquals(estacion.getDireccion(), "Manacor", 0);
+        assertEquals(estacion.getNumeroAnclajes(), 6);
     }
 
 }
