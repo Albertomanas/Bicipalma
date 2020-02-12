e083630793c1782d0b222de11d37840ab00a0dca
Albertomanas <amanas@cifpfbmoll.eu>
2020-02-12 03:42:05 +0100

    test(BicicletaTest): ComprobarIdBicicletaTest

---
+++ Bici_palma/src/test/java/edu/elsmancs/Bici_palma/domain/BicicletaTest.java
@@ -0,0 +1,14 @@
+package edu.elsmancs.Bici_palma.domain;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class BicicletaTest {
+
+    @Test
+    public void comprobarIdBicicletaTest() {
+        Bicicleta bicicleta = new Bicicleta(234);
+        assertEquals(234, 234);
+    }
+
+}
