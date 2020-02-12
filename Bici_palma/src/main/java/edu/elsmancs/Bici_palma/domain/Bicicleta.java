fe6084745654f9a21de463d9e592f8af33e08818
Albertomanas <amanas@cifpfbmoll.eu>
2020-02-12 03:46:55 +0100

    feat(Bicicleta): Implementación de getter id

--- Bici_palma/src/main/java/edu/elsmancs/Bici_palma/domain/Bicicleta.java
+++ Bici_palma/src/main/java/edu/elsmancs/Bici_palma/domain/Bicicleta.java
@@ -1,5 +1,13 @@
 package edu.elsmancs.Bici_palma.domain;
 
 public class Bicicleta {
+    private int id = 0; //limitado a 3 dígitos.
 
+    public Bicicleta(int id) { // Método constructor.
+         this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
 }
